a54b6578e3094054dac7d09466d425416f3557ea
deshaeb <butlerdeshae@gmail.com>
2024-09-17 16:27:26 -0400

    Updated ensureLoggedIn

--- backend/routes/books.js
+++ backend/routes/books.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const booksCtrl = require('../controllers/books');
 const reviewsCtrl = require('../controllers/reviews');
+const ensureLoggedIn = require('../middleware/ensureLoggedIn')
 
 //all paths start w/'api/books'
 
@@ -9,24 +10,24 @@ const reviewsCtrl = require('../controllers/reviews');
 router.get('/', booksCtrl.index);
 
 // GET /books/:bookId --> SHOW FUNCTIONALITY 
-router.get('/:bookId', booksCtrl.show);
+router.get('/:bookId', ensureLoggedIn, booksCtrl.show);
 
 // POST /books --> CREATE FUNCTIONALITY 
-router.post('/', booksCtrl.create); 
+router.post('/', ensureLoggedIn, booksCtrl.create); 
 
 // POST /books/:bookId/reviews --> review --- CREATE FUNCTIONALITY
-router.post('/:bookId/reviews', reviewsCtrl.create); 
+router.post('/:bookId/reviews', ensureLoggedIn, reviewsCtrl.create); 
 
 // PUT /books/:bookId --> UPDATE FUNCTIONALITY
-router.put('/:bookId', booksCtrl.update); 
+router.put('/:bookId', ensureLoggedIn, booksCtrl.update); 
 
 // PUT /books/:bookId/reviews/:review Id --> review - UPDATE FUNCTIONALITY 
-router.put('/:bookId/reviews/:reviewId', reviewsCtrl.update);
+router.put('/:bookId/reviews/:reviewId', ensureLoggedIn, reviewsCtrl.update);
 
 // DELETE /books/:bookId --> DELETE FUNCTIONALITY
-router.delete('/:bookId',booksCtrl.booksDelete); 
+router.delete('/:bookId', ensureLoggedIn,booksCtrl.booksDelete); 
 
 // DELETE 
-router.delete('/:bookId/reviews/:reviewId', reviewsCtrl.deleteReview); 
+router.delete('/:bookId/reviews/:reviewId', ensureLoggedIn, reviewsCtrl.deleteReview); 
 
 module.exports = router;
